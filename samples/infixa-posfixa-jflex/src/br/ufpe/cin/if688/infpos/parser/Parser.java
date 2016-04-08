cf75e998bdd6b8ea6878fc7c3a9c8eaf4e4564f7
Igor Matos <igormattos.ea@gmail.com>
2016-04-08 08:18:53 -0300

    added samples

---
+++ samples/infixa-posfixa-jflex/src/br/ufpe/cin/if688/infpos/parser/Parser.java
@@ -0,0 +1,75 @@
+package br.ufpe.cin.if688.infpos.parser;
+
+import java.io.IOException;
+
+import br.ufpe.cin.if688.infpos.lexer.InfPosLexer;
+import br.ufpe.cin.if688.infpos.lexer.Num;
+import br.ufpe.cin.if688.infpos.lexer.Tag;
+import br.ufpe.cin.if688.infpos.lexer.Token;
+import br.ufpe.cin.if688.infpos.lexer.Word;
+
+public class Parser {
+   private InfPosLexer lex;    // analisador lexico
+   private Token look;   // lookahead tag
+	
+	public Parser(InfPosLexer l) throws IOException { 
+		lex = l; move(); 
+	}
+
+	void move() throws IOException { 
+		look = lex.yylex(); 
+	}
+	
+	void error(String s) { 
+		throw new Error("problema de parsing"); 
+	}
+
+	
+	public void expr() throws IOException {
+		term();
+		move(); 
+		while(true) {
+			if (null == look) {
+				return;
+			}
+			else if (look.tag == '+') {
+				match('+'); 
+				term(); 
+				System.out.print("+ ");
+				move(); 
+			}
+			else if (look.tag == '-') {
+				match('-'); 
+				term(); 
+				System.out.print("- ");
+				move(); 
+			}
+			else return;
+		}
+	}
+	void term() throws IOException {
+		switch( look.tag ) {
+			case Tag.NUM:
+				Num n = (Num) look;
+				System.out.print(n.getValue() + " ");
+				break;
+			case Tag.ID:
+				Word w = (Word) look;
+				System.out.print(w.getLexeme() + " ");
+				break;
+			default:
+				error("erro de sintaxe");
+				break;
+		}
+	}
+	
+	void match(int t) throws IOException {
+		if( look.tag == t ) {
+			move();
+		}
+		else {
+			error("erro de sintaxe");
+		}
+	}
+
+}
